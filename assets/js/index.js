d070831a16fc7f27e29e5faf7ed99c17aa7b92e2
bgshendraa_ <bagushendraa.27@gmail.com>
2025-08-03 01:14:22 +0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,17 @@
 document.getElementById("menuToggle").addEventListener("click", function () {
   document.getElementById("navLinks").classList.toggle("show");
 });
+
+  const text = "Semoga di hari istimewa ini kamu diberikan kebahagiaan, banyak rezeki, dan sehat selalu 💖";
+  const typingText = document.getElementById("typing-text");
+  let index = 0;
+
+  function typeWriter() {
+    if (index < text.length) {
+      typingText.innerHTML += text.charAt(index);
+      index++;
+      setTimeout(typeWriter, 60); // kecepatan mengetik (ms)
+    }
+  }
+
+  typeWriter();
